2307a0fe153f42f432bde9cb5c93b564692fdcec
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-08-10 12:23:25 +0800

    fix

--- src/main/java/org/thinkingstudio/forgeutils/ForgeUtils.java
+++ src/main/java/org/thinkingstudio/forgeutils/ForgeUtils.java
@@ -1,12 +1,11 @@
 package org.thinkingstudio.forgeutils;
 
-import com.ibm.icu.impl.Pair;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
-
 import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.thinkingstudio.forgeutils.api.Mods;
 import org.thinkingstudio.forgeutils.impl.ModsImpl;
 
